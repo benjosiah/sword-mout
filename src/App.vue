3621335185f066d6feeb2d2b7fd0a2989155d5a8
ben josiah <benjosiah90@gmail.com>
2021-04-10 12:52:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <home/>
+</template>
+
+<script>
+import Home from "./components/Home.vue"
+export default {
+  name:"App",
+  components:{
+    "home":Home
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+
+
+</script>
+
+<style>
+body{
+  margin: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,101 @@
+<template>
+
+  <!-- landing page -->
+  <div class="m-0 flex w-full flex-wrap items-center justify-around border-solid border-0 border-b border-black">
+    <div class="md:w-2/5 sm:w-full text-center px-1">
+      <h1>
+        Write, Read and Publish your own Gospel Articles
+      </h1>
+    </div>
+    <div class="md:w-7/12 sm:w-full">
+      <img class="w-full "  src=".././assets/images/5836.jpg" alt="">
+    </div>
+  </div>
+
+  <!-- what is  -->
+  <div class="flex  flex-wrap items-center justify-center mt-3 border-solid border-0 border-b border-black">
+    <div class="md:w-1/5 sm:w-5/6 p-10 rounded-md border border-solid m-1">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ad eum inventore facilis ea iusto vero commodi ducimus a? Laborum.
+    </div>
+    <div class="md:w-1/5 sm:w-4/12 m-1" >
+       <img class="md:w-full sm:w-1/2 md:h-auto"  src=".././assets/images/5465.jpg" alt="">
+    </div>
+  </div>
+
+  <!-- illustration -->
+  <div class="flex flex-wrap items-center justify-center border-solid border-0 border-b border-black lg:pt-8 lg:pb-8">
+   
+     <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" sm:w-2/3 md:h-auto"  src=".././assets/images/10720.jpg" alt="">
+        <div>Writting</div>
+    </div>
+      <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-2/3 h-40"  src=".././assets/images/3529668-removebg-preview.png" alt="">
+       <div>Reading</div>
+    </div>
+    <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-2/3 h-auto"  src=".././assets/images/19333428.jpg" alt="">
+        <div>Ask Questions</div>
+    </div>
+    
+  </div>
+
+  <!-- what  we do -->
+  <div class="flex flex-wrap items-center justify-around mt-20">
+       <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/3529668.jpg" alt="">
+       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat.</div>
+    </div>
+     <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/10720.jpg" alt="">
+        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, beatae.</div>
+    </div>
+    <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/19333428.jpg" alt="">
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, eos!</div>
+    </div>
+       <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/3529668.jpg" alt="">
+       <div>Reading</div>
+    </div>
+     <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/10720.jpg" alt="">
+        <div>Writting</div>
+    </div>
+    <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/19333428.jpg" alt="">
+        <div>Ask Questions</div>
+    </div>
+       <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/3529668.jpg" alt="">
+       <div>Reading</div>
+    </div>
+     <div class="md:w-1/5 sm:w-2/5 m-1" >
+       <img class=" w-3/4 md:h-auto"  src=".././assets/images/10720.jpg" alt="">
+        <div>Writting</div>
+    </div>
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  
+  name:"Home",
+  components:{
+
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
